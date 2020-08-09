0abaa52ffa16fe40fdae2a90f85f339ceb7ed652
Thanh Tran <tranthanhdeveloper@gmail.com>
2020-08-09 16:58:39 +0700

    add banner.txt, clean the source code

--- src/main/java/com/programmingsharing/basicauthentication/conf/SecurityConf.java
+++ src/main/java/com/programmingsharing/basicauthentication/conf/SecurityConf.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 
 @Configuration
 public class SecurityConf extends WebSecurityConfigurerAdapter {
@@ -18,7 +16,6 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
                 .withUser("demo").password("$2y$12$6Yv1a0vXp3ZlAS2LuhR24.qUkLWUjKztMWsPj9aTZnj5sDx7cSeqK").roles("ADMIN")
                 .and().withUser("invalid").password("$2y$12$hM1GfM8C.hxYm5Pjv1Go3.HNIYkegcaUJrsRd/ejOClOBzY7xcuHa").roles();
-
     }
 
     @Override
